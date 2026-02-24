46115493124409678ee7cc9dbcfbc4408d129a46
Matheus <rmatheusluiz@gmail.com>
2026-02-23 22:05:46 -0300

    feat: cria entidade user no model

---
+++ src/main/java/dev/matheus/ecommerce/Model/User.java
@@ -0,0 +1,33 @@
+package dev.matheus.ecommerce.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_user")
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "Formato do e-mail inválido.")
+    private String email;
+}
