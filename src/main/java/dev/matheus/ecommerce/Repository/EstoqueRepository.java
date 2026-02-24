0bb549c9525df58e4c7a559c7827a72570b0ae6b
Matheus <rmatheusluiz@gmail.com>
2026-02-23 22:12:48 -0300

    feat: implementa repository para entidade estoque

---
+++ src/main/java/dev/matheus/ecommerce/Repository/EstoqueRepository.java
@@ -0,0 +1,9 @@
+package dev.matheus.ecommerce.Repository;
+
+import dev.matheus.ecommerce.Model.Estoque;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstoqueRepository  extends JpaRepository<Estoque, Long> {
+}
