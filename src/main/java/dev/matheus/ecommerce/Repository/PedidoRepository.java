4a79dfe04c839daf5f7a0198c33d6405539ee00f
Matheus <rmatheusluiz@gmail.com>
2026-02-23 22:14:11 -0300

    feat: implementa repository para entidade pedido

---
+++ src/main/java/dev/matheus/ecommerce/Repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package dev.matheus.ecommerce.Repository;
+
+import dev.matheus.ecommerce.Model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
