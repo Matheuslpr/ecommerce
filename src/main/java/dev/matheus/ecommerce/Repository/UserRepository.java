2d5daa6ff2fe6be3b80f84a85f573f0e2f8542a1
Matheus <rmatheusluiz@gmail.com>
2026-02-23 22:15:19 -0300

    feat: implementa repository para entidade User

---
+++ src/main/java/dev/matheus/ecommerce/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.matheus.ecommerce.Repository;
+
+import dev.matheus.ecommerce.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+     User findByEmail(String email);
+}
