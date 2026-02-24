125aafb8090ed51d3038010eab73128a5fd9af32
Matheus <rmatheusluiz@gmail.com>
2026-02-23 22:02:36 -0300

    feat: cria a entidade produto no model

---
+++ src/main/java/dev/matheus/ecommerce/Model/Produto.java
@@ -0,0 +1,35 @@
+package dev.matheus.ecommerce.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produtos")
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeProduto;
+
+    private String descricao;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+}
