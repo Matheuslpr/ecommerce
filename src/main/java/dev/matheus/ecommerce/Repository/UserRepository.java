c281d0ec78270cf4a7d01bee874223467e2a2bc7
Matheus <rmatheusluiz@gmail.com>
2026-03-01 13:44:51 -0300

    fix: implementa o userDetails

--- src/main/java/dev/matheus/ecommerce/Repository/UserRepository.java
+++ src/main/java/dev/matheus/ecommerce/Repository/UserRepository.java
@@ -2,9 +2,11 @@ package dev.matheus.ecommerce.Repository;
 
 import dev.matheus.ecommerce.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-     User findByEmail(String email);
+    UserDetails findByEmail(String email);
 }
